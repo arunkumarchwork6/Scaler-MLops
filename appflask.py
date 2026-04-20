1b90a84e4bba569eed83670e58aae1e12a82f446
arunkumarchwork6 <arunkumarchwork6@gmail.com>
2026-04-21 00:24:14 +0530

    withfast

---
+++ appflask.py
@@ -0,0 +1,18 @@
+from flask import Flask
+
+
+app = Flask(__name__)
+
+
+@app.route("/ping",methods = ['GET'])
+def ping():
+    return "I am working fine"
+
+@app.route("/",methods = ['GET'])
+def homeping():
+    return "I am in home page"
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
